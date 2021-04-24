d26f34963ea91bf0c22303bda2f9c99788a0d5ff
reedleoneil <reedleoneilpascual@yahoo.com>
2021-04-24 17:59:27 +0800

    updated script

--- public/convert.rb
+++ public/convert.rb
@@ -1,19 +1,24 @@
 is_meaning = false
 
-File.foreach("D:/pon-jp-cheat-sheet/cheat-sheet/kanji/n3/set-1.markdown") do |line, line_no| 
-    if line.include?("# ") && line.include?("<ruby>") == false
-        puts "-" # write - kanji: 
-        puts "  kanji: " + line.delete("#").strip
-        is_meaning = true
-    elsif is_meaning
-        puts "  meaning: " + line.strip
-        is_meaning = false
-        puts "  onyomi: "
-    elsif line.include?("|")
-        l = line.split("|")
-        puts "    -"
-        puts "      kanji: " + l[0].strip
-        puts "      kana: " + l[1].strip
-        puts "      meaning: " + l[2].strip
+def file_append (line)
+    File.open("kanji-n3.yml", "a"){|f| f.puts(line)}
+end
+
+(1..9).each do |x|
+    File.foreach("D:/pon-jp-cheat-sheet/cheat-sheet/kanji/n3/set-" + x.to_s + ".markdown") do |line, line_no| 
+        if line.include?("# ") && line.include?("<ruby>") == false
+            file_append "-" # write - kanji: 
+            file_append "  kanji: " + line.delete("#").strip
+            is_meaning = true
+        elsif is_meaning
+            is_meaning = false
+            file_append "  onyomi: "
+        elsif line.include?("|")
+            l = line.split("|")
+            file_append "    -"
+            file_append "      kanji: " + l[1].strip
+            file_append "      kana: " + l[2].strip
+            file_append "      meaning: " + l[3].strip
+        end
     end
 end
